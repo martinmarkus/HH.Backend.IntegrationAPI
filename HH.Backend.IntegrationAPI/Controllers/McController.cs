d38ad0a89920dc2c7ceaa4c0287e0bd02d1e6cb1
Martin Márkus <martinmarkus0@gmail.com>
2023-12-07 19:33:46 +0100

    McController

---
+++ HH.Backend.IntegrationAPI/Controllers/McController.cs
@@ -0,0 +1,32 @@
+﻿using HH.Backend.Common.API.Services;
+using HH.Backend.Common.Core.DTOs.RequestDTOs;
+using HH.Backend.Common.Core.DTOs.ResponseDTOs;
+using HH.Backend.Common.Web.Attributes;
+using HH.Backend.Common.Web.Controllers;
+using HH.Backend.IntegrationAPI.Request.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HH.Backend.IntegrationAPI.Controllers
+{
+{
+    [ServiceFilter(typeof(AuthorizeIntegration))]
+    [IgnoreClientValidation]
+    [ApiExplorerSettings(IgnoreApi = true)] // INFO: Swagger ignore
+    public class McController : BaseAPIController
+    {
+        private readonly IMcService _mcService;
+
+        public McController(IMcService mcService) : base()
+        {
+            _mcService = mcService;
+        }
+
+        [HttpPost]
+        public async Task<LoginFromMcResponseDTO> LoginFromMcAsync(LoginFromMcRequestDTO dto) =>
+            await _mcService.LoginFromMcAsync(dto);
+
+        [HttpPost]
+        public async Task<RegisterFromMcResponseDTO> RegisterFromMcAsync(RegisterFromMcRequestDTO dto) =>
+            await _mcService.RegisterFromMcAsync(dto);
+    }
+}
